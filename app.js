687a7199b121c8fb1bbf1bb0efbcbddd2f253dea
PrateekP <prateekpanta1789@gmail.com>
2025-05-20 23:29:51 +0530

    Added basic App class

---
+++ app.js
@@ -0,0 +1,8 @@
+class App {
+    constructor() {
+        this.item1 = document.querySelector(".");
+        this.item2 = document.querySelector(".");
+        this.item3 = document.querySelector(".");
+        this.init();
+    };
+}
