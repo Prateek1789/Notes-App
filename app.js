35769ff182610798487b4f4147a09d5dcee9fc40
PrateekP <prateekpanta1789@gmail.com>
2025-06-11 21:54:47 +0530

    Added mock fucntionality for testing

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
 class App {
     constructor() {
-        this.addNoteBtn = document.querySelector(".head");
+        this.addNoteBtn = document.querySelector(".add-note-btn");
+        this.noteArea = document.querySelector(".notes-area");
+        this.noteColourOptions = document.querySelectorAll(".clr-circle");
         this.init()
     };
     init() {
-        this.show();
+        this.appFunctions();
     }
-    show() {
+    appFunctions() {
+        const note = `<div class="note">
+                        <div class="note-header">
+                            <span class="note-date">09-06-2025</span>
+                            <h3>Homework</h3>
+                        </div>
+                        <textarea name="content" id="note-1" class="note-content" autofocus="on"></textarea>
+                        <div class="note-footer">
+                            <span class="note-time">08:08PM, </span>
+                            <span class="note-day">Monday</span>
+                        </div>
+                      </div>`;
+
         this.addNoteBtn.addEventListener("click", () => {
-            document.querySelector(".add-note-tab").classList.toggle("active");
-            document.querySelectorAll(".new-note-option").forEach(itm => {
-                itm.classList.toggle("active");
-            });
+            this.noteArea.innerHTML += note;
+        });
+
+        const noteContent = document.querySelectorAll(".note-content");
+        noteContent.forEach(itm => {
+            itm.addEventListener("keydown", (e) => {
+                    if (e.key === "Enter") {
+                        itm.setAttribute("disabled", "");
+                }
+            })
         })
     }
 }
