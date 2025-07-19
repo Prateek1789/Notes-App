13e96d956194099485ec111154f38384f0c7a680
PrateekP <prateekpanta1789@gmail.com>
2025-07-19 21:16:51 +0530

    Shifted update method directly into Manager class

--- JS/DataModel/Note.js
+++ JS/DataModel/Note.js
@@ -1,21 +1,14 @@
 import DateUtility from "../Utils/DateUtility.js";
 
 class Note {
-    constructor(title = '', content = '', color) {
-        this.id = 'n_' + Date.now().toString(36).slice(-6);
+    constructor(id, title = '', content = '', color, createdAt, createdOn) {
+        this.id = id || 'n_' + Date.now().toString(36).slice(-6);
         this.title = title;
         this.content = content;
         this.color = color;
         this.isTrashed = false;
-        this.createdOn = DateUtility.getCurrentDate().date;
-        this.createdAt = DateUtility.getCurrentDate().time;
-        this.updatedOn = DateUtility.getCurrentDate().date;
-        this.updatedAt = DateUtility.getCurrentDate().time;
-    };
-
-    update(title, content) {
-        this.title = title;
-        this.content = content;
+        this.createdOn = createdOn || DateUtility.getCurrentDate().date;
+        this.createdAt = createdAt || DateUtility.getCurrentDate().time;
         this.updatedOn = DateUtility.getCurrentDate().date;
         this.updatedAt = DateUtility.getCurrentDate().time;
     };
