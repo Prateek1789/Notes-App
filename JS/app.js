630441a17a1f89c9a9ba39309d875e9fcd71507a
PrateekP <prateekpanta1789@gmail.com>
2025-07-22 15:39:22 +0530

    Enhanced search bar activation logic to be more simple, removed unnecessary conditions and refactored code for more modular and cleaner structure

--- JS/app.js
+++ JS/app.js
@@ -12,7 +12,7 @@ class NotesApp {
         this.inTrash = false;
         this.isEditing = false;
         this.editingNote;
-        this.elements;
+        this.domRef;
         this.timer;
         this.manager = new Manager();
         this.ui = new NoteUI(this.manager);
@@ -20,12 +20,12 @@ class NotesApp {
     }
 
     initApp() {
-        this.elements = this.getAppElements();
+        this.domRef = this.getDOMReference();
         this.initAppEvents();
         this.tabName.textContent = "Home";
     }
 
-    getAppElements() {
+    getDOMReference() {
         return {
             searchBar: document.querySelector('.search-container'),
             searchInput: document.querySelector('#search'),
@@ -41,32 +41,20 @@ class NotesApp {
     initAppEvents() {
         /* Storage.clear(); */
         this.displayNotesForActiveTab();
+
+        // Event Listener for Search Bar
+        this.activateSearchEvents();
+
         // Event Listener for KEYDOWN events 
-        document.addEventListener("keydown", (e) => {
-            if (e.metaKey && e.key === "k") {
-                e.preventDefault();
-                this.toggleSearchVisibility(true, this.elements.searchBar, this.elements.searchInput, this.elements.searchShortcut);
-            }
-
-            if (e.key === "Escape" && 
-                this.elements.searchBar.classList.contains("active") && 
-                this.elements.searchShortcut.classList.contains("hide") && 
-                this.elements.searchInput.value === "") {
-
-                this.toggleSearchVisibility(false, this.elements.searchBar, this.elements.searchInput, this.elements.searchShortcut);
-            }
-        });
-
-        // Event listener for Search Events
-        this.elements.searchInput.addEventListener('input', () => this.performNoteSearch());
+        this.activateKeyboardEvents();
 
         // Event Listeners for CLICK events
+        this.activateClickEvents();
+    };
+
+    activateClickEvents() {
         document.addEventListener("click", (e) => {
-            if (e.target.closest(".search-container")) this.toggleSearchVisibility(true, this.elements.searchBar, this.elements.searchInput, this.elements.searchShortcut);
-
-            if (!e.target.closest(".search-container")) this.toggleSearchVisibility(false, this.elements.searchBar, this.elements.searchInput, this.elements.searchShortcut);
-
-            if (e.target.closest(".theme-toggle")) this.switchTheme(this.elements.themeToggle);
+            if (e.target.closest(".theme-toggle")) this.switchTheme(this.domRef.themeToggle);
 
             if (e.target.closest(".btn-add-note") && !this.inTrash) this.showNoteModal();
 
@@ -94,6 +82,35 @@ class NotesApp {
 
             if (e.target.closest(".btn-restore")) this.restoreTrashedNotes(e);
         });
+    }
+
+    activateKeyboardEvents() {
+        document.addEventListener("keydown", (e) => {
+            if (e.metaKey && e.key === "k") {
+                e.preventDefault(); 
+                this.domRef.searchInput.focus();
+            }
+        
+            if (e.key === "Escape" && this.domRef.searchInput.value === "") this.domRef.searchInput.blur();
+        });
+    }
+
+    activateSearchEvents() {
+        this.domRef.searchInput.addEventListener('focus', () => this.activateSearch());
+
+        this.domRef.searchInput.addEventListener('blur', () => this.deactivateSearch());
+
+        this.domRef.searchInput.addEventListener('input', () => this.performNoteSearch());
+    };
+
+    activateSearch() {
+        this.domRef.searchBar.classList.add('active');
+        this.domRef.searchShortcut.classList.add('hide');
+    };
+
+    deactivateSearch() {
+        this.domRef.searchBar.classList.remove('active');
+        this.domRef.searchShortcut.classList.remove('hide');
     };
 
     switchTheme(toggle) {
@@ -106,7 +123,7 @@ class NotesApp {
         let color = '';
 
         if (!this.isEditing) {
-            const checkedInput = this.elements.colorOptions.find(itm => itm.checked);
+            const checkedInput = this.domRef.colorOptions.find(itm => itm.checked);
             if (checkedInput) {
                 const label = checkedInput.parentElement;
                 const colorProperty = label.dataset.color;
@@ -123,20 +140,20 @@ class NotesApp {
     };
 
     getNoteModalInputs() {
-        const noteTitle = this.elements.dialog.querySelector("#note-form-heading");
-        const noteContent = this.elements.dialog.querySelector("#note-form-text");
-        const noteTags = this.elements.dialog.querySelector("#note-tags");
+        const noteTitle = this.domRef.dialog.querySelector("#note-form-heading");
+        const noteContent = this.domRef.dialog.querySelector("#note-form-text");
+        const noteTags = this.domRef.dialog.querySelector("#note-tags");
 
         return [noteTitle, noteContent, noteTags];
     };
 
     showNoteModal() {
-        const formHead = this.elements.dialog.querySelector(".form-head");
+        const formHead = this.domRef.dialog.querySelector(".form-head");
         const formInputs = this.getNoteModalInputs();
         let noteColor =  this.getNoteColor();
 
         formHead.style.backgroundColor = noteColor;
-        this.elements.dialog.showModal();
+        this.domRef.dialog.showModal();
         formInputs[0].focus();
 
         if (this.isEditing) {
@@ -144,13 +161,13 @@ class NotesApp {
             formInputs[1].value = this.editingNote.querySelector("textarea").value;
             formInputs[2].value = this.manager.getAll().find(note => note.id === this.editingNote.dataset.id).tags;
         }
-    }
+    };
 
     closeNoteModal(elm1, elm2, elm3) {
         elm1.value = '';
         elm2.value = '';
         elm3.value = '';
-        this.elements.dialog.close();
+        this.domRef.dialog.close();
         this.isEditing = false;
         this.editingNote = '';
     };
@@ -173,12 +190,12 @@ class NotesApp {
         this.isEditing = true;
         this.editingNote = e.target.closest('.note');
         this.showNoteModal();
-    }
+    };
 
     bookmarkNotes(event) {
         const btn = event.target.closest(".btn-star");
-        btn.classList.toggle("active");
-    }
+        btn.classList.toggle("marked");
+    };
 
     updateNote(title, content, tags) {
         const noteID = this.editingNote.dataset.id;
@@ -190,7 +207,7 @@ class NotesApp {
         
         this.manager.update(noteID, title.value, content.value, tags.value);
         this.closeNoteModal(title, content, tags);
-    }
+    };
 
     deleteNote(event) {
         const parent = event.target.closest(".note");
@@ -199,13 +216,13 @@ class NotesApp {
         if (!this.inTrash) this.manager.softDelete(parent.dataset.id);
 
         if (this.inTrash) this.manager.hardDelete(parent.dataset.id);
-    }
+    };
 
     restoreTrashedNotes(event) {
         const parent = event.target.closest(".note");
         parent.remove();
         this.manager.restoreNote(parent.dataset.id);
-    }
+    };
 
     switchToTab(event) {
         if (event.target.closest('.home-btn')) {
@@ -220,47 +237,34 @@ class NotesApp {
             this.tabName.textContent = "Trash";
         }
         this.displayNotesForActiveTab();
-    }
-
-    toggleSearchVisibility(isActive, element, input, shortcut) {
-        if (isActive) {
-            element.classList.add('active');
-            shortcut.classList.add('hide');
-            input.focus();
-        }
-        else {
-            element.classList.remove('active');
-            shortcut.classList.remove('hide');
-            input.blur();
-        }
-    }
+    };
 
     performNoteSearch() {
         const delay = 750;
         clearTimeout(this.timer);
         this.timer = setTimeout(() => {
-            const query = this.elements.searchInput.value;
+            const query = this.domRef.searchInput.value;
             const result = this.manager.search(query);
             this.noteArea.innerHTML = '';
             result.forEach(note => this.ui.renderNote(note, this.noteArea));
         }, delay);
-    }
+    };
 
     /* clearAllNotes() {
 
     } */
 
     updateActiveTabButton() {
-        if (this.inHome && !this.elements.btnHome.classList.contains("btn-active")) {
-            this.elements.btnHome.classList.add("btn-active");
-            this.elements.btnTrash.classList.remove("btn-active");
+        if (this.inHome && !this.domRef.btnHome.classList.contains("btn-active")) {
+            this.domRef.btnHome.classList.add("btn-active");
+            this.domRef.btnTrash.classList.remove("btn-active");
         }
         
-        if (this.inTrash && !this.elements.btnTrash.classList.contains("btn-active")) {
-            this.elements.btnTrash.classList.add("btn-active");
-            this.elements.btnHome.classList.remove("btn-active");
+        if (this.inTrash && !this.domRef.btnTrash.classList.contains("btn-active")) {
+            this.domRef.btnTrash.classList.add("btn-active");
+            this.domRef.btnHome.classList.remove("btn-active");
         }
-    }
+    };
 
     displayNotesForActiveTab() {
         if (this.inHome && !this.inTrash) this.ui.renderMainNotes(this.noteArea);
@@ -268,11 +272,11 @@ class NotesApp {
         if (this.inTrash && !this.inHome) this.ui.renderDeletedNotes(this.noteArea);
 
         this.updateActiveTabButton();
-    }
+    };
 
     saveNotes() {
         this.manager.saveNotes();
-    }
+    };
 }
 
 const App = new NotesApp();
