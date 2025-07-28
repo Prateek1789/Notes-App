3e7ff0f1c69708e9cccc0e64f09d113ea2f73a02
PrateekP <prateekpanta1789@gmail.com>
2025-07-28 20:43:51 +0530

    Removed redundant methods and refactored code to query unified storage

--- JS/Manager/NotesManager.js
+++ JS/Manager/NotesManager.js
@@ -4,7 +4,6 @@ import NotesStorage from '../Data/Storage.js';
 class NotesManager {
     constructor() {
         this.notes = [];
-        this.deletedNotes = [];
     };
 
     create(title, content, tags, color) {
@@ -16,92 +15,69 @@ class NotesManager {
     };
 
     update(id, title, content, tags) {
-        const savedNotes = this.getAll();
+        const savedNotes = this.getAllNotes();
         const noteIndex = savedNotes.findIndex(note => note.id === id);
 
         if (noteIndex > -1) {
             const note = savedNotes[noteIndex];
             savedNotes[noteIndex] = new Note(id, title, content, tags, note.color, note.createdAt, note.createdOn);
-            NotesStorage.save('notes', savedNotes);
+            NotesStorage.save(savedNotes);
         };
         return null;
     };
 
-    getAll() {
-        return NotesStorage.load('notes');
-    };
-
-    getDeletedNotes() {
-        return NotesStorage.load('trash');
+    getAllNotes() {
+        return NotesStorage.load();
     };
 
     saveNotes() {
-        const savedData = this.getAll();
-        savedData.push(...this.notes);
-        NotesStorage.save('notes', savedData);
+        const savedNotes = this.getAllNotes();
+        savedNotes.push(...this.notes);
+        NotesStorage.save(savedNotes);
 
         this.notes = [];
     };
 
-    setTrash(deletedNotes) {
-        const savedTrash = this.getDeletedNotes();
-        savedTrash.push(...deletedNotes);
-        NotesStorage.save('trash', savedTrash);
-
-        this.deletedNotes = [];
-    };
-
     softDelete(id) {
-        const savedNotes = this.getAll();
+        const savedNotes = this.getAllNotes();
         const noteIndex = savedNotes.findIndex(note => note.id === id);
+        savedNotes[noteIndex].isTrashed = true;
 
-        this.deletedNotes.push(savedNotes[noteIndex]);
-        savedNotes[noteIndex] = Note.createSkeleton(id);
-
-        NotesStorage.save('notes', savedNotes);
-        this.setTrash(this.deletedNotes);
+        NotesStorage.save(savedNotes);
     };
 
     hardDelete(id) {
-        const savedNotes = this.getAll();
-        const trashNotes = this.getDeletedNotes();
-
+        const savedNotes = this.getAllNotes();
         const mainIndex = savedNotes.findIndex(note => note.id === id);
-        const trashIndex = trashNotes.findIndex(note => note.id === id);
 
-        if (mainIndex > -1 && trashIndex > -1) {
+        if (mainIndex > -1) {
             savedNotes.splice(mainIndex, 1);
-            trashNotes.splice(trashIndex, 1);
-            NotesStorage.save('notes', savedNotes);
-            NotesStorage.save('trash', trashNotes);
+            NotesStorage.save(savedNotes);
         }
 
         return null;
     };
 
     restoreNote(id) {
-        const savedNotes = this.getAll();
-        const trashNotes = this.getDeletedNotes();
+        const savedNotes = this.getAllNotes();
         const indexMain = savedNotes.findIndex(note => note.id === id);
-        const note = trashNotes.find(note => note.id === id);
-        savedNotes[indexMain] = note;
+        savedNotes[indexMain].isTrashed = false;
 
-        NotesStorage.save('notes', savedNotes);
-        NotesStorage.save('trash', trashNotes.filter(note => note.id !== id));
+        NotesStorage.save(savedNotes);
     }
 
     geSearchParameters(query, tab) {
         const searchTerm = query.toLowerCase().trim();
 
         if (tab === 'dashboard') {
-            const notes = this.getAll().filter(note => !note.isTrashed);
+            const notes = this.getAllNotes().filter(note => !note.isTrashed);
             if (!query) return notes;
 
             return this.search(searchTerm, notes);
         }
         
         if (tab === 'trash') {
-            const notes = this.getDeletedNotes();
+            const notes = this.getAllNotes().filter(note => note.isTrashed);
             if (!query) return notes;
 
             return this.search(searchTerm, notes);
